0531b1fabb8f564158a04620d3cbbab50ea50958
Georges Mbatou Charles DIEME <georgesmbatoucharles@gmail.com>
2022-12-19 00:19:00 +0000

    Commit "welcome page" //comment have to fix some scss and understand justify content and items //the routing is not working have to fix with Bright NEW !! Clean up some scss with the login page and Clean module.ts

--- polley-app/src/app/app-routing.module.ts
+++ polley-app/src/app/app-routing.module.ts
@@ -15,6 +15,14 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
 ];
 
 @NgModule({
---
+++ polley-app/src/app/welcome/welcome.module.ts
@@ -0,0 +1,22 @@
+import { InputModule } from './../component/input/input.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { WelcomePageRoutingModule } from './welcome-routing.module';
+
+import { WelcomePage } from './welcome.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    InputModule,
+    WelcomePageRoutingModule
+  ],
+  declarations: [WelcomePage]
+})
+export class WelcomePageModule {}
