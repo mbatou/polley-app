8b3119877a4429811b2f40c03195496ece1e7d2c
Georges Mbatou Charles DIEME <georgesmbatoucharles@gmail.com>
2022-12-19 15:30:56 +0000

    Testing new components on home.page // not working implementing the cards manually so

--- polley-app/src/app/app.module.ts
+++ polley-app/src/app/app.module.ts
@@ -8,9 +8,9 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent], 
 })
 export class AppModule {}
